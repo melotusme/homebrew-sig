1dc4afecff7c8919adde2af6f39753791288805d
zhirong <hanzhirong@kuaishou.com>
2020-05-08 14:01:04 +0800

    u

--- Formula/sig.rb
+++ Formula/sig.rb
@@ -5,7 +5,11 @@ class Sig < Formula
   sha256 "14b2d4435ac6395d9155d0fe8ec3905ef6d74ba8da1f396af56b9a10e1104d10"
   version "0.0.1"
 
+
+  depends_on "go" => :build
+
   def install
+    system 'make dep && make build_bin'
     bin.install Dir["bin/*"]
   end
 end

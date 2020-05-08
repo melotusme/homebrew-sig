b82a238a87f5f92d00bfa82496495ee6aab53801
zhirong <hanzhirong@kuaishou.com>
2020-05-08 11:14:50 +0800

    add formula

---
+++ Formula/sig.rb
@@ -0,0 +1,11 @@
+class Sig < Formula
+  desc "A Simple Sig CLI"
+  homepage "https://github.com/melotusme/homebrew-sig"
+  url "https://github.com/melotusme/sig/archive/0.0.1.zip"
+  version "0.0.1"
+
+  def install
+    bin.install Dir["bin/*"]
+    bin
+  end
+end
